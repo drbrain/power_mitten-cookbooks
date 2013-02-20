4123b6dfeaecdf93f54424a0606a64ddd682761b
Eric Hodel <drbrain@segment7.net>
2013-02-20 14:44:26 -0800

    Install ncurses before ruby
    This allows the gauntlet console to work

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -7,15 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node['ruby_build']['install_pkgs_cruby'] << 'libncurses5'
-node['ruby_build']['install_pkgs_cruby'] << 'libncurses5-dev'
-
 ruby_version = node['ruby_version']
 
 ruby_build_ruby ruby_version
 
-node['ruby_path'] =
+node.set['ruby_path'] =
   File.join node['ruby_build']['default_ruby_base_path'], ruby_version
 
-node['gem_path'] = File.join node['ruby_path'], 'bin/gem'
+node.set['gem_path'] = File.join node['ruby_path'], 'bin/gem'
 
