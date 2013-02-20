bdc517f0aeea37ac74863321f5e4421c79b836ea
Eric Hodel <drbrain@segment7.net>
2013-02-20 13:20:15 -0800

    Use released rubygems 1.8.25 for update

--- cookbooks/rubygems/recipes/default.rb
+++ cookbooks/rubygems/recipes/default.rb
@@ -2,26 +2,14 @@
 # Cookbook Name:: rubygems
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, AT&T
 #
 # All rights reserved - Do Not Redistribute
 #
-cookbook_file '/tmp/rubygems-update.tar.gz' do
-  source 'rubygems-1.8.25.tgz'
-  mode 0644
-end
 
-ruby = File.join node['ruby_path'], 'bin/ruby'
-
-execute 'Unpack rubygems-update' do
-  cwd '/tmp'
-
-  command 'tar xzf rubygems-update.tar.gz'
-end
+gem = node['gem_path']
 
 execute 'Update rubygems' do
-  cwd '/tmp/rubygems-1.8.25'
-
-  command "#{ruby} setup.rb --no-rdoc --no-ri"
+  command "#{gem} update --system"
 end
 
