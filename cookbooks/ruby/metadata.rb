8dc11a90c9d9cd8daa8c260580821e3e89689b3f
Eric Hodel <drbrain@segment7.net>
2013-02-22 14:56:37 -0800

    ruby cookbook depends on ruby_build

--- cookbooks/ruby/metadata.rb
+++ cookbooks/ruby/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
+maintainer       "AT&T"
 maintainer_email "eh0377@att.com"
 license          "All rights reserved"
 description      "Installs/Configures ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "ruby_build"
