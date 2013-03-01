ca52e74f6364817d2274250027c31b4e5707104c
Eric Hodel <drbrain@segment7.net>
2013-03-01 14:38:44 -0800

    Use RDoc gem

--- cookbooks/rdoc/recipes/default.rb
+++ cookbooks/rdoc/recipes/default.rb
@@ -7,12 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-cookbook_file '/tmp/rdoc.gem' do
-  source 'rdoc.gem'
-  mode 0644
-end
+#cookbook_file '/tmp/rdoc.gem' do
+#  source 'rdoc.gem'
+#  mode 0644
+#end
+#
+#execute 'install rdoc' do
+#  command "#{node['gem_path']} install /tmp/rdoc.gem -q --no-rdoc --no-ri"
+#end
 
-execute 'install rdoc' do
-  command "#{node['gem_path']} install /tmp/rdoc.gem -q --no-rdoc --no-ri"
+gem_package 'rdoc' do
+  version '>= 4'
+  options '--force'
+  gem_binary node['gem_path']
+
+  action :upgrade
 end
 
