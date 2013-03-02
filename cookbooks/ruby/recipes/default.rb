c8ae003040b449b15fe68fe8d122706497d185de
Eric Hodel <drbrain@segment7.net>
2013-03-01 18:25:22 -0800

    Add ruby to the default PATH

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -11,8 +11,22 @@ ruby_version = node['ruby_version']
 
 ruby_build_ruby ruby_version
 
-node.set['ruby_path'] =
+ruby_path = 
   File.join node['ruby_build']['default_ruby_base_path'], ruby_version
 
+node.set['ruby_path'] = ruby_path
+
 node.set['gem_path'] = File.join node['ruby_path'], 'bin/gem'
+
+file '/etc/profile.d/ruby_path.sh' do
+  mode 0755
+  owner 'root'
+  action :create
+
+  content <<-CONTENT
+#!/bin/sh
+
+export PATH="#{ruby_path}/bin:$PATH"
+  CONTENT
+end
 
